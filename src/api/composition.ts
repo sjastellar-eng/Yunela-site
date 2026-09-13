4c1a20437d0431526f3e2bdef9c460cf78579a15
sjastellar-eng <sja.stellar@gmail.com>
2026-09-14 01:25:49 +0300

    feat: activate recommendation engine in API composition

--- src/api/composition.ts
+++ src/api/composition.ts
@@ -1,11 +1,12 @@
 import { CustomerService } from '../application/customer/service';
 import { FeedbackService } from '../application/feedback/service';
 import { TeaProfileService } from '../application/profile/service';
-import { NotConfiguredRecommendationEngine, RecommendationApplicationService } from '../application/recommendation/service';
+import { DeterministicRecommendationEngine, RecommendationApplicationService } from '../application/recommendation/service';
 import { TeaService } from '../application/tea/service';
 import {
   SqliteCustomerRepository,
   SqliteFeedbackRepository,
+  SqliteRecommendationHistoryRepository,
   SqliteTeaProfileRepository,
   SqliteTeaRepository,
 } from '../application/sqliteRepositories';
@@ -18,12 +19,18 @@ export function createDefaultApiServer(databaseFile = process.env.YUNELA_DB_FILE
   applyMigrations(db);
   const teaRepository = new SqliteTeaRepository(db);
   const customerRepository = new SqliteCustomerRepository(db);
+  const profileRepository = new SqliteTeaProfileRepository(db);
+  const feedbackRepository = new SqliteFeedbackRepository(db);
+  const historyRepository = new SqliteRecommendationHistoryRepository(db);
   const dependencies: ApiDependencies = {
     teaService: new TeaService(teaRepository),
     customerService: new CustomerService(customerRepository),
-    profileService: new TeaProfileService(new SqliteTeaProfileRepository(db), customerRepository),
-    feedbackService: new FeedbackService(new SqliteFeedbackRepository(db), customerRepository, teaRepository),
-    recommendationService: new RecommendationApplicationService(new NotConfiguredRecommendationEngine()),
+    profileService: new TeaProfileService(profileRepository, customerRepository),
+    feedbackService: new FeedbackService(feedbackRepository, customerRepository, teaRepository),
+    recommendationService: new RecommendationApplicationService(
+      new DeterministicRecommendationEngine(teaRepository),
+      { teaRepository, customerRepository, historyRepository },
+    ),
   };
   return { server: createApiServer({ dependencies }), close: () => db.close() };
 }
