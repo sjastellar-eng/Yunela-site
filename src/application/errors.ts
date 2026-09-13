5719cb43a33e8633384327ed985eb464a1d00743
sjastellar-eng <sja.stellar@gmail.com>
2026-09-14 00:02:20 +0300

    feat: add C1 application error model

---
+++ src/application/errors.ts
@@ -0,0 +1,27 @@
+export type ApplicationErrorCode =
+  | 'VALIDATION_ERROR'
+  | 'NOT_FOUND'
+  | 'CONFLICT'
+  | 'DOMAIN_RULE_VIOLATION'
+  | 'PERSISTENCE_ERROR'
+  | 'UNEXPECTED_ERROR'
+  | 'NOT_IMPLEMENTED';
+
+export class ApplicationError extends Error {
+  constructor(
+    public readonly code: ApplicationErrorCode,
+    message: string,
+    options?: { cause?: unknown },
+  ) {
+    super(message, options);
+    this.name = 'ApplicationError';
+  }
+}
+
+export function isApplicationError(error: unknown): error is ApplicationError {
+  return error instanceof ApplicationError;
+}
+
+export function toPersistenceError(error: unknown, message = 'Persistence operation failed'): ApplicationError {
+  return new ApplicationError('PERSISTENCE_ERROR', message, { cause: error });
+}
