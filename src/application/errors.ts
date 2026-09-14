e5412b0da0fca5a958ae1a9abc320733724709bf
sjastellar-eng <sja.stellar@gmail.com>
2026-09-14 23:22:56 +0300

    feat(D2): add commerce application error boundaries

--- src/application/errors.ts
+++ src/application/errors.ts
@@ -6,14 +6,24 @@ export type ApplicationErrorCode =
   | 'DISCOVERY_BOX_INSUFFICIENT_CANDIDATES'
   | 'PERSISTENCE_ERROR'
   | 'UNEXPECTED_ERROR'
-  | 'NOT_IMPLEMENTED';
+  | 'NOT_IMPLEMENTED'
+  | 'CART_NOT_FOUND'
+  | 'CART_NOT_OWNED'
+  | 'CART_ITEM_NOT_FOUND'
+  | 'INVALID_QUANTITY'
+  | 'INVALID_SKU'
+  | 'PRODUCT_NOT_FOUND'
+  | 'PRODUCT_UNAVAILABLE'
+  | 'INSUFFICIENT_INVENTORY'
+  | 'ORDER_NOT_FOUND'
+  | 'ORDER_NOT_OWNED'
+  | 'ORDER_IDEMPOTENCY_CONFLICT'
+  | 'INVALID_CURRENCY'
+  | 'PURCHASE_ALREADY_RECORDED'
+  | 'PURCHASE_NOT_AUTHORIZED';
 
 export class ApplicationError extends Error {
-  constructor(
-    public readonly code: ApplicationErrorCode,
-    message: string,
-    options?: { cause?: unknown },
-  ) {
+  constructor(public readonly code: ApplicationErrorCode, message: string, options?: { cause?: unknown }) {
     super(message, options);
     this.name = 'ApplicationError';
   }
